b87e79cdc879b867391b74026c18d47ade552aee
mohammad <mavaddathossien@gmail.com>
2025-07-08 15:21:15 +0330

    HI

---
+++ bank/login.cpp
@@ -0,0 +1,15 @@
+#include "login.h"
+#include "ui_login.h"
+#include<iostream>
+login::login(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::login)
+{
+    ui->setupUi(this);
+}
+
+login::~login()
+{
+    delete ui;
+}
+
